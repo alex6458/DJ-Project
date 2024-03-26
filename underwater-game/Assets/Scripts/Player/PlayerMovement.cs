e87fae2d3d32355a25cfeba8e13e28351779d1a9
Alexandre van Velze <54676747+alex6458@users.noreply.github.com>
2024-03-26 22:36:06 +0000

    Unity Project Files

---
+++ underwater-game/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float speed;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+    }
+}
