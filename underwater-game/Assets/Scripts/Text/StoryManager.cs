db29fa138255bc94bcc201e02923a3647c08b375
André Moreira <andre-f-moreira@hotmail.com>
2024-06-05 16:27:35 +0100

    final Wave

--- underwater-game/Assets/Scripts/Text/StoryManager.cs
+++ underwater-game/Assets/Scripts/Text/StoryManager.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class StoryManager : MonoBehaviour
 {
     public GameObject[] popUps;
-    private int popUpIndex;
+    public GameObject[] finalDialogue;
+    public int popUpIndex = 0;
+    private int DialogueIndex = 0;
     private bool storyShown = false;
-
-
+    public SpawnCreatures spawnScript;
+    private bool FinalWaveSpawned = false;
     void Update()
     {
 
@@ -23,7 +26,38 @@ public class StoryManager : MonoBehaviour
             }
         }
 
+        if (popUpIndex == popUps.Length)
+        {
+            for (int i = 0; i < finalDialogue.Length; i++)
+            {
+                if (i == DialogueIndex)
+                    finalDialogue[i].SetActive(true);
+                else
+                    finalDialogue[i].SetActive(false);
+            }
+
+
+            if (!PauseMenu.IsPaused)
+            {
+                if (DialogueIndex < finalDialogue.Length)
+                {
+                    //If player clicks Enter, Spacebar or LeftClick he goes to next dialogue box
+                    if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+                    {
+                        DialogueIndex++;
+                    }
+                }
+            }
+
+            if (DialogueIndex >= finalDialogue.Length && !FinalWaveSpawned)
+            {
+                FinalWaveSpawned = true;
+                StartCoroutine(spawnScript.FinalWave());
+            }
+        }
+
     }
+
 
     public void ShowStory()
     {
